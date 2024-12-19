83bdc04e04295bda36f9d47123de8c0e5534b3ad
Abylay <mrmongtv@mail.ru>
2024-12-19 13:53:08 +0500

    Backend

---
+++ src/main/java/Shop/Online_Shop/model/User.java
@@ -0,0 +1,26 @@
+package Shop.Online_Shop.model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+@Component
+public class User extends BaseModel {
+    private String email;
+    private int age;
+    private String fullName;
+    private String password;
+    private double balance;
+}
---
+++ src/main/java/Shop/Online_Shop/repository/BagRepository.java
@@ -0,0 +1,13 @@
+package Shop.Online_Shop.repository;
+
+import Shop.Online_Shop.model.Bag;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Transactional
+@Repository
+public interface BagRepository extends JpaRepository<Bag,Long> {
+    Bag findByUserIsCartId(Long userId);
+}
